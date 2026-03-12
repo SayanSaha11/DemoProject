aeb4907cf57016201deeaf5e2a541a3710b68e9c
sayansaha <sayansaha00876@gmail.com>
2026-03-12 14:27:03 +0530

    Second commit

---
+++ 19.RESTassured/src/test/java/Shopper/Concept.java
@@ -0,0 +1,20 @@
+package Shopper;
+
+import org.testng.annotations.Test;
+
+public class Concept {
+	@Test
+	public void btest() {
+		System.out.println("Test B passed");
+	}
+	
+	@Test(dependsOnMethods="btest")
+	public void atest() {
+		System.out.println("Test A passed");
+	}
+	
+	@Test(dependsOnMethods="atest", invocationCount = 5, enabled = false)
+	public void ctest() {
+		System.out.println("Test C passed");
+	}
+}
